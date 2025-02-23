0f70f07afafb7d5ee2fe62ccf30afa46b5e06c8e
Omarmoatz <omar.raouf.002@gmail.com>
2025-02-23 12:55:24 +0200

    jagged arrays

--- array/Arrays.java
+++ array/Arrays.java
@@ -2,8 +2,21 @@ package array;
 
 public class Arrays {
     public static void main(String a[]){
-        int arr[][] = new int[3][4];
+        int arr[][] = new int[3][];
 
+        arr[0] = new int[4];
+        arr[1] = new int[5];
+        arr[2] = new int[6];
+
+
+        for(int n[] : arr){
+            for(int m:n){
+                m = (int)(Math.random()*10);
+                System.out.print(m + " ");
+            }
+
+            System.out.println(" ");
+        }
 
         for(int i=0; i<3; i++){
 
@@ -13,17 +26,6 @@ public class Arrays {
             }
             System.out.println(" ");
         }
-
-
-        for(int n[] : arr){
-            for(int m:n){
-                System.out.print(m + " ");
-            }
-
-            System.out.println(" ");
-        }
-
-
 
 
 
